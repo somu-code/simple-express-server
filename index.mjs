cb996c9d7e6af9a972307830c7cbd197d0cf5d82
Somnath Golui <code.somu@gmail.com>
2023-08-13 16:07:24 +0530

    update

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.send("<h1>Hello, from express.</h1>");
+  res.send("<h1>Hello, from express. deploy.sh script is running.</h1>");
 });
 
 app.listen(3000, () => {
